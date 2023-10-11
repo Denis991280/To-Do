f2cbf30b0f5b1a688acd43fa8cb586b31ff57151
unknown <denis.hadzipasic@partyrent.com>
2023-10-11 17:54:08 +0200

    a

--- ToDo/script2.js
+++ ToDo/script2.js
@@ -120,6 +120,35 @@ function myFunction() {
   }
 }
 
+const input = document.getElementById("searchInput")
+
+input.addEventListener("keyup", () => {
+    const filter = input.value.toUpperCase();
+
+    const paragraphs = document.querySelectorAll(".input-controller");
+
+    if(filter === "") {
+      paragraphs.forEach(p => {
+        p.innerHTML = p.textContent;
+      })
+      return;
+    }
+    paragraphs.forEach(p => {
+
+      let txtValue = p.textContent
+
+      if(txtValue.toUpperCase().indexOf(filter) > -1) {
+        txtValue = txtValue.replace(
+          new RegExp(filter, "gi"),
+          '<span class="highlight">$&</span>'
+        );
+        p.innerHTML = txtValue;
+      } else {
+        p.innerHTML = txtValue;
+      }
+    })
+})
+
 function refreshFunction() {
   location.reload(true);
 }
